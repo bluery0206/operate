49559b6a01f12368d751a2dfd672d8cbe88f56b5
aori___ <hilariomarkryan3@gmail.com>
2024-11-15 22:00:00 +0800

    updated html, utils, added format_text, generate_docx, save_docx

--- home/utils.py
+++ home/utils.py
@@ -1,7 +1,14 @@
+from django.http import HttpResponse
 from PIL import Image
 from django.utils import timezone
+import string
+from docx.shared import Inches
+from docx import Document
+from pathlib import Path
 
-
+cwd_path = Path().cwd()
+media_path = cwd_path.joinpath("media")
+outputs_path = media_path.joinpath("outputs")
 
 def get_full_name(profile:object, m_initial:bool=True):
 	full_name = profile.l_name
@@ -19,6 +26,13 @@ def get_full_name(profile:object, m_initial:bool=True):
 
 	return full_name
 
+def format_text(text):
+	symbols = string.punctuation.replace('_', ' ')
+	translation_table = str.maketrans(symbols, '_' * len(symbols))
+
+	text = text.replace(",", "")
+	text = text.translate(translation_table)
+	return text
 
 def save_profile_picture(profile):	
 	ori_img = Image.open(profile.raw_image.path)
@@ -58,3 +72,47 @@ def get_img_size(width, height, size) -> list:
 
 	return [left, top, right, bottom]
 
+def generate_docx(template_path, image_path, fields, data):
+	doc = Document(template_path)
+
+	# inserts the image
+	for p in doc.paragraphs:
+		if "raw_image" in p.text:
+			p.clear()
+			run = p.add_run()
+			run.add_picture(
+				image_path, 
+				width=Inches(2), 
+				height=Inches(2)
+			)
+			p.alignment = 1
+
+	# fills in the table
+	for table in doc.tables:
+		for row in table.rows:
+			cell = row.cells[1]
+			for paragraph in cell.paragraphs:
+				for field, value in zip(fields, data):
+					if field in paragraph.text:
+						for run in paragraph.runs:
+							if field in run.text:
+								run.text = run.text.replace(field, str(value))
+								run.font.name = 'Arial'
+
+	file_name	= f"{format_text(data[-1])}.docx"
+	save_path	= outputs_path.joinpath(file_name)
+	doc.save(save_path)
+
+	return [file_name, save_path]
+
+def save_docx(file_name, save_path):
+	with open(save_path, 'rb') as f:
+		response = HttpResponse(
+			f.read(),
+			content_type='application/vnd.openxmlformats-officedocument.wordprocessingml.document'
+		)
+		response['Content-Disposition'] = f'attachment; filename="{file_name}"'
+	
+	save_path.unlink(save_path)
+
+	return response
