d4d6d43c925e2bedb758d837a4ac14b0d607d8c8
aori___ <hilariomarkryan3@gmail.com>
2024-11-15 22:01:25 +0800

    Fixed urls, shorten logic and generalized all methods. A whole update basically execpt for personnels and inmates

--- profiles/urls.py
+++ profiles/urls.py
@@ -6,27 +6,15 @@ urlpatterns = [
 	path("inmates/", views.inmates, name="profiles-inmates"),
 
 
-
 	path("<str:p_type>/<int:pk>/", views.profile, name="profile"),
+	path("<str:p_type>/add/", views.profile_add, name="profile-add"),
 	path("<str:p_type>/<int:pk>/edit/", views.profile_update, name="profile-update"),
-    
+	path("<str:p_type>/<int:pk>/delete/", views.profile_delete, name="profile-delete"),
+	path("<str:p_type>/<int:pk>/download/", views.profile_docx_download, name="profile-docx-download"),
 
-    
-	path("personnel/<int:pk>/", views.profile_personnel, name="profile-personnel"),
-	path("inmate/<int:pk>/", views.profile_inmate, name="profile-inmate"),
 
-	path("personnel/<int:pk>/update/", views.profile_personnel_update, name="profile-personnel-update"),
-	path("inmate/<int:pk>/update/", views.profile_inmate_update, name="profile-inmate-update"),
 
-	path("personnel/add/", views.profile_personnel_add, name="profile-personnel-add"),
-	path("inmate/add/", views.profile_inmate_add, name="profile-inmate-add"),
-
-	path("personnel/delete/<int:pk>/", views.profile_personnel_delete, name="profile-personnel-delete"),
-	path("inmate/delete/<int:pk>/", views.profile_inmate_delete, name="profile-inmate-delete"),
 
 	path("generate/inmate/docx/<int:pk>", views.profile_inmate_to_docx, name="profile-inmate-to-docx"),
 	path("generate/personnel/docx/<int:pk>", views.profile_personnel_to_docx, name="profile-personnel-to-docx"),
-
-	path("personnel/delete/all/", views.delete_all_personnel, name="delete-all-personnel"),
-	path("inmate/delete/all/", views.delete_all_inmate, name="delete-all-inmate"),
 ] 
--- profiles/views.py
+++ profiles/views.py
@@ -1,5 +1,3 @@
-from django.http import HttpResponse
-from django.conf import settings
 from django.db.models import Q
 from django.shortcuts import (
 	render, 
@@ -7,16 +5,10 @@ from django.shortcuts import (
 	get_object_or_404
 )	
 
-from docx.enum.text import WD_PARAGRAPH_ALIGNMENT
-from docx.shared import Inches
-from docx import Document
-
 from datetime import datetime
-from pathlib import Path
-import os
 
 from .models import Personnel, Inmate, Template
-from home.utils import save_profile_picture, get_full_name
+from home.utils import save_profile_picture, get_full_name, generate_docx, save_docx
 from .forms import (
 	CreatePersonnel, 
 	CreateInmate, 
@@ -25,12 +17,9 @@ from .forms import (
 	TemplateUploadForm
 )
 
-cwd_path = Path().cwd()
-media_path = cwd_path.joinpath("media")
-outputs_path = media_path.joinpath("outputs")
 
-if not outputs_path.exists():
-	outputs_path.mkdir(exist_ok=False) 
+
+
 
 def personnels(request):
 	sort_choices = [
@@ -106,6 +95,9 @@ def personnels(request):
 	return render(request, "profiles/personnels.html", context)
 
 
+
+
+
 def inmates(request):
 	sort_choices = [
 		['l_name', "Last Name"],
@@ -170,145 +162,8 @@ def inmates(request):
 		context['filters'].update({"search": search})
 	return render(request, "profiles/inmates.html", context)
 
- 
-def profile_personnel(request, pk):
-	context = {
-		'profile': get_object_or_404(Personnel, pk=pk),
-	}
-	return render(request, "profiles/profile_personnel.html", context)
- 
-
-def profile_inmate(request, pk):
-	context = {
-		'profile': get_object_or_404(Inmate, pk=pk),
-	}
-	return render(request, "profiles/profile_inmate.html", context)
 
 
-def profile_personnel_update(request, pk):
-	profile = get_object_or_404(Personnel, pk=pk)
-
-	if request.method == "POST":
-		form = UpdatePersonnel(request.POST, request.FILES, instance=profile)
-
-		if form.is_valid():
-			instance = form.save()
-
-			# For updating profile picture
-			if 'raw_image' in request.FILES:
-				save_profile_picture(instance)
-
-			return redirect('profile-personnel', pk)
-	else:
-		form = UpdatePersonnel(instance=profile)
-
-	context = {
-		'prev'		: request.GET.get("prev", ""),
-		'profile'	: profile,
-		'form'		: form,
-	}
-	return render(request, "profiles/profile_update.html", context)
-
-
-def profile_inmate_update(request, pk):
-	profile = get_object_or_404(Inmate, pk=pk)
-
-	if request.method == "POST":
-		form = UpdateInmate(request.POST, request.FILES, instance=profile)
-
-		if form.is_valid():
-			instance = form.save()
-
-			# For updating profile picture
-			if 'raw_image' in request.FILES:
-				save_profile_picture(instance)
-
-			return redirect('profile-inmate', pk)
-	else:
-		form = UpdateInmate(instance=profile)
-
-	context = {
-		'prev'		: request.GET.get("prev", ""),
-		'profile'	: profile,
-		'form'		: form,
-	}
-	return render(request, "profiles/profile_update.html", context)
-
-
-def profile_inmate_add(request):
-	prev = request.GET.get("prev", "")
-
-	if request.method == "POST":
-		form = CreateInmate(request.POST, request.FILES)
-
-		if form.is_valid():
-			instance = form.save()
-			if 'raw_image' in request.FILES:
-				save_profile_picture(instance)
-			return redirect(prev)
-	else:
-		form = CreateInmate()
-
-	context = {
-		'form' : form,
-	}
-	return render(request, "profiles/profile_add.html", context)
-
-
-def profile_personnel_add(request):
-	prev = request.GET.get("prev", "")
-
-	if request.method == "POST":
-		form = CreatePersonnel(request.POST, request.FILES)
-
-		if form.is_valid():
-			instance = form.save()
-			if 'raw_image' in request.FILES:
-				save_profile_picture(instance)
-			return redirect(prev)
-	else:
-		form = CreatePersonnel()
-
-	context = {
-		'form' : form,
-	}
-	return render(request, "profiles/profile_add.html", context)
-
-
-def profile_personnel_delete(request, pk):
-	prev	= request.GET.get("prev", "")
-	profile = get_object_or_404(Personnel, pk=pk)
-
-	if request.method == "POST":
-		profile.delete()
-
-		if prev:
-			return redirect(prev)
-		else:
-			return redirect('profiles-personnels')
-
-	context = {
-		'profile' : profile,
-	}
-	return render(request, "profiles/profile_delete_confirm.html", context)
-
-
-def profile_inmate_delete(request, pk):
-	prev	= request.GET.get("prev", "")
-	profile = get_object_or_404(Inmate, pk=pk)
-
-	if request.method == "POST":
-		profile.delete()
-
-		if prev:
-			return redirect(prev)
-		else:
-			return redirect('profiles-inmates')
-
-	context = {
-		'profile' : profile,
-	}
-	return render(request, "profiles/profile_delete_confirm.html", context)
 
 
 def profile_template_upload(request):
@@ -325,194 +180,49 @@ def profile_template_upload(request):
 	}
 	return render(request, "profiles/profile_template_upload.html", context)
 
-def profile_inmate_to_docx(request, pk):
-	template	= Template.objects.get(template_name__icontains="inmate")
-	profile		= Inmate.objects.get(pk=pk)
-
-	fields = [
-		'[[name]]',
-		'[[age]]',
-		'[[address]]',
-		'[[civil_status]]',
-		'[[date_arrested]]',
-		'[[date_committed]]',
-		'[[crime_violated]]'
-	]
-
-	if profile.date_arrested != None:
-		date_arrested = datetime.fromisoformat(str(profile.date_arrested))
-		date_arrested = date_arrested.strftime("%B %d, %Y, %I:%M %p")
-	else:
-		date_arrested = profile.date_arrested
-
-	if profile.date_committed != None:
-		date_committed = datetime.fromisoformat(str(profile.date_committed))
-		date_committed = date_committed.strftime("%B %d, %Y, %I:%M %p")
-	else:
-		date_committed = profile.date_committed
-
-	data = [
-		get_full_name(profile),
-		profile.age,
-		profile.address,
-		profile.civil_status.replace("_", " ").title(),
-		date_arrested,
-		date_committed,
-		profile.crime_violated
-	]
-
-	doc = Document(template.template_file.path)
-
-	for p in doc.paragraphs:
-		if "raw_image" in p.text:
-			p.clear()
-
-			run = p.add_run()
-			run.add_picture(
-				profile.raw_image.path, 
-				width=Inches(2), 
-				height=Inches(2)
-			)
-			
-			p.alignment = 1
-
-	for table in doc.tables:
-		for row in table.rows:
-			cell = row.cells[1]
-			# Check each paragraph in the second cell for placeholders
-			for paragraph in cell.paragraphs:
-				for field, value in zip(fields, data):
-					if field in paragraph.text:
-						paragraph.text = paragraph.text.replace(field, str(value))
-	name		= str(get_full_name(profile, True))
-	file_name	= f"{name}.docx"
-	save_path	= outputs_path.joinpath(file_name)
-	doc.save(save_path)
-
-	with open(save_path, 'rb') as f:
-		response = HttpResponse(
-			f.read(),
-			content_type='application/vnd.openxmlformats-officedocument.wordprocessingml.document'
-		)
-		response['Content-Disposition'] = f'attachment; filename="{file_name}.docx"'
-	os.remove(save_path)
-	return response
 
 
-def profile_personnel_to_docx(request, pk):
-	template	= Template.objects.get(template_name__icontains="personnel")
-	profile		= Personnel.objects.get(pk=pk)
-
-	fields = [
-		'[[rank]]',
-		'[[name]]',
-		'[[age]]',
-		'[[address]]',
-		'[[civil_status]]',
-		'[[date_assigned]]',
-		'[[date_relieved]]',
-		'[[designation]]'
-	]
-
-	if profile.date_assigned != None:
-		date_assigned = datetime.fromisoformat(str(profile.date_assigned))
-		date_assigned = date_assigned.strftime("%B %d, %Y, %I:%M %p")
-	else:
-		date_assigned = profile.date_assigned
-
-	if profile.date_relieved != None:
-		date_relieved = datetime.fromisoformat(str(profile.date_relieved))
-		date_relieved = date_relieved.strftime("%B %d, %Y, %I:%M %p")
-	else:
-		date_relieved = profile.date_relieved
-
-	data = [
-		profile.rank.upper(),
-		get_full_name(profile),
-		profile.age,
-		profile.address,
-		profile.civil_status.replace("_", " ").title(),
-		date_assigned,
-		date_relieved,
-		profile.designation
-	]
-
-	doc = Document(template.template_file.path)
-
-	for p in doc.paragraphs:
-		if "raw_image" in p.text:
-			p.clear()
-
-			run = p.add_run()
-			run.add_picture(
-				profile.raw_image.path, 
-				width=Inches(2), 
-				height=Inches(2)
-			)
-			
-			p.alignment = 1
-
-	for table in doc.tables:
-		for row in table.rows:
-			cell = row.cells[1]
-			# Check each paragraph in the second cell for placeholders
-			for paragraph in cell.paragraphs:
-				for field, value in zip(fields, data):
-					if field in paragraph.text:
-						paragraph.text = paragraph.text.replace(field, str(value))
-
-	name		= str(get_full_name(profile, True))
-	file_name	= f"{name}.docx"
-	save_path	= outputs_path.joinpath(file_name)
-	doc.save(save_path)
-	
-
-	with open(save_path, 'rb') as f:
-		response = HttpResponse(
-			f.read(),
-			content_type='application/vnd.openxmlformats-officedocument.wordprocessingml.document'
-		)
-		response['Content-Disposition'] = f'attachment; filename="{file_name}.docx"'
-	os.remove(save_path)
-	return response
 
 
-def delete_all_inmate(request):
-	prev 	= request.GET.get("prev", "")
-
-	if request.method == "POST":
-		Inmate.objects.all().delete()
-		return redirect(prev)
-
-	context = {
-		'profile' : "all profiles",
-	}
-	return render(request, "profiles/profile_delete_confirm.html", context)
-
-
-def delete_all_personnel(request):
-	prev 	= request.GET.get("prev", "")
-
-	if request.method == "POST":
-		Personnel.objects.all().delete()
-		return redirect(prev)
-
-	context = {
-		'profile' : "all profiles",
-	}
-	return render(request, "profiles/profile_delete_confirm.html", context)
-
-
- 
 def profile(request, p_type, pk):
 	p_class =  Personnel if p_type == "personnel" else Inmate
-	profile	=  get_object_or_404(p_class, pk=pk)
+	
+	data = [field.name for field in p_class._meta.get_fields()]
+	print(data)
 
 	context = {
-		'profile': profile,
+		'profile': get_object_or_404(p_class, pk=pk),
 	}
 	return render(request, "profiles/profile.html", context)
+
+
+
+
  
+def profile_add(request, p_type):
+	create_profile_form = CreatePersonnel if p_type == "personnel" else CreateInmate
+
+	if request.method == "POST":
+		form = create_profile_form(request.POST, request.FILES)
+
+		if form.is_valid():
+			instance = form.save()
+
+			if 'raw_image' in request.FILES: save_profile_picture(instance)
+
+			return redirect('profile', p_type, instance.pk)
+	else:
+		form = create_profile_form()
+
+	context = {
+		'form' : form,
+	}
+	return render(request, "profiles/profile_add.html", context)
+
+
+
+
+
 def profile_update(request, p_type, pk):
 	p_class, update_form = [Personnel, UpdatePersonnel] if p_type == "personnel" else [Inmate, UpdateInmate]
 	profile = get_object_or_404(p_class, pk=pk)
@@ -523,7 +233,6 @@ def profile_update(request, p_type, pk):
 		if form.is_valid():
 			instance = form.save()
 
-			# For updating profile picture
 			if 'raw_image' in request.FILES: save_profile_picture(instance)
 
 			return redirect('profile', p_type, pk)
@@ -536,3 +245,82 @@ def profile_update(request, p_type, pk):
 		'form'		: form,
 	}
 	return render(request, "profiles/profile_update.html", context)
+
+
+
+
+
+def profile_delete(request, p_type, pk):
+	prev = request.GET.get("prev", "")
+
+	p_class = Personnel if p_type == "personnel" else Inmate
+	profile = get_object_or_404(p_class, pk=pk)
+
+	if request.method == "POST":
+		profile.delete()
+
+		return redirect(prev) if prev else redirect(f"profiles-{p_type}s")
+
+	context = {
+		'title' 	: f"Delete {get_full_name(profile)}'s profile?",
+		'warning' 	: f"You can't retrieve this profile once its deleted. Why not archive it first if you're not sure and haven't done it yet?",
+	}
+	return render(request, "home/confirmation_page.html", context)
+
+
+
+
+
+def delete_all(request, p_type):
+	prev 	= request.GET.get("prev", "")
+	p_class = Personnel if p_type == "personnel" else Inmate
+
+	if request.method == "POST":
+		p_class.objects.all().delete()
+
+		return redirect(prev)
+
+	context = {
+		'title' 	: f"Delete all profile?",
+		'warning' 	: f"You can't retrieve all profiles once they're deleted. Why not archive them first if you're not sure and haven't done it yet?",
+	}
+	return render(request, "home/confirmation_page.html", context)
+
+
+
+
+
+def profile_docx_download(request, p_type, pk):
+	p_class = Personnel if p_type == "personnel" else Inmate
+
+	template	= Template.objects.get(template_name__icontains=p_type)
+	profile		= p_class.objects.get(pk=pk)
+
+	fields = [field.name for field in profile._meta.get_fields()]
+	fields.append("full_name")
+	fields = [f"[[{field}]]" for field in fields]
+	
+	data = {field.name: getattr(profile, field.name, None) for field in profile._meta.get_fields()}
+	data["full_name"] = get_full_name(profile)
+
+	time_format = "%B %d, %Y"
+
+	for key, value in data.items():
+		if "date" in key and value:
+			data[key] = datetime.fromisoformat(str(data[key])).strftime(time_format).upper()
+		elif type(value) == str:
+			data[key] = value.upper()
+
+	data = [value for key, value in data.items()]
+	print(f"{data=}")
+
+	file_name, save_path = generate_docx(
+		template_path	= template.template_file.path, 
+		image_path		= profile.raw_image.path, 
+		fields 			= fields, 
+		data 			= data
+	)
+
+	response = save_docx(file_name, save_path)
+	
+	return response
