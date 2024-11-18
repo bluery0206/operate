7d5253eda577c1336745c1007777e6c57447959b
aori___ <hilariomarkryan3@gmail.com>
2024-11-19 01:08:31 +0800

    Update dialogs (login, logout, forgotpassword, etc. any user related actions/views)

--- home/templatetags/custom_filters.py
+++ home/templatetags/custom_filters.py
@@ -6,3 +6,9 @@ register = template.Library()
 @register.filter
 def full_name(profile:object, m_initial:bool=True):
 	return get_full_name(profile, m_initial)
+
+
+@register.filter
+def add_class(field, css_class):
+    return field.as_widget(attrs={"class": css_class})
+
