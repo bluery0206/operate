838cc35886313c6e3cbbdf9d380c426750c6d755
aori___ <hilariomarkryan3@gmail.com>
2025-01-20 02:49:36 +0800

    Removed `DETECTION_AS_ONNX` option in `get_model()` as it cant be access in first initialization of the system as apps needed to be initializing by django first.

--- operate/model_loader.py
+++ operate/model_loader.py
@@ -12,7 +12,6 @@ logger = logging.getLogger(__name__)
 class ModelType(Enum):
 	EMBEDDING_GENERATOR = 0
 	DETECTION = 1
-	DETECTION_AS_ONNX = 2
 
 def get_model(type:ModelType):
 	defset = OPERATE_SETTINGS.objects.first()
@@ -34,15 +33,8 @@ def get_model(type:ModelType):
 			model = cv2.FaceDetectorYN.create(
 				model = defset.model_detection.path,
 				config = "",
-				input_size = (defset.clip_size, defset.clip_size)
+				input_size = (defset.bbox_size, defset.bbox_size)
 			)
-		elif type == ModelType.DETECTION_AS_ONNX:
-			if not defset.model_detection: 
-				raise ModelNotFoundError("Face Detection model not found.")
-			
-			session_options = ort.SessionOptions()
-			session_options.graph_optimization_level = ort.GraphOptimizationLevel.ORT_ENABLE_ALL
-			model = ort.InferenceSession(defset.model_detection.path, sess_options=session_options)
 		else:
 			exception_message = f"No model represents \"{type}\"."
 			logger.exception(exception_message)
